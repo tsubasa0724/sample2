28f0f2c9313e726f688b749d623212cf694df8dc
tsubasa <wingman0724@gmail.com>
2024-03-12 11:00:41 +0900

    初回コミット

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿namespace App4;
+
+class Animal
+{
+    public string Name { get; private set; }    // 名前
+    public int Age { get; private set; }        // 年齢
+
+    public Animal(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    public virtual void Speak()
+    {
+        Console.WriteLine("......");
+    }
+
+    public void ShowProfile()
+    {
+        Console.WriteLine(Name + "," + Age + "歳");
+    }
+}
+class Cat : Animal
+{
+    public Cat(string name, int age) : base(name, age)
+    {
+
+    }
+    public void Sleep()
+    {
+        Console.WriteLine("スースー");
+    }
+    public override void Speak()
+    {
+        Console.WriteLine("ニャー");
+    }
+}
+class Dog : Animal
+{
+    public Dog(string name, int age) : base(name, age)
+    {
+
+    }
+    public void Run()
+    {
+        Console.WriteLine("トコトコ");
+    }
+    public override void Speak()
+    {
+        Console.WriteLine("ワンワン");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal[] animal = { new Cat("くわた", 24), new Dog("つばさ", 25), new Cat("くわた", 23), new Dog("つばさ", 26) };
+        foreach (Animal a in animal)
+        {
+            a.ShowProfile();
+            a.Speak();
+        }
+    }
+}
